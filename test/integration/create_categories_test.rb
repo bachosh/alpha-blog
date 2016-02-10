73e84c3c648fab7452227dff11b30b3dcc6183da
bacho <bachosh2@gmail.com>
2016-02-10 09:32:32 +0000

    category with tests

---
+++ test/integration/create_categories_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class CreateCategoriesTest < ActionDispatch::IntegrationTest
+  
+  test "get new category  form and create category" do
+      get new_category_path # vamowmebt  links
+    
+      assert_template 'categories/new' # vamowmebt templates html.erb files 
+    
+      assert_difference 'Category.count', 1 do   #gansxvaveba unda iyos 'category.count'  da  1 shoris
+     
+      post_via_redirect categories_path, category: {name: "sports"} # post_via_redirect aris http post request to category_path, gadavcemt category.names mnishvnelobit
+     
+      
+      assert_template 'categories/index' # vamowmebt templates html.erb files 
+      
+      assert_match "sports", response.body   # zemot mocemuli tamplate is body shi tu aris "sports"
+      
+      end  
+  end  
+  
+  test "invalid category submission results in failure" do
+      get new_category_path
+      assert_template 'categories/new'
+          assert_no_difference 'Category.count' do
+          post categories_path, category: {name: " "}
+      end
+      assert_template 'categories/new'
+        assert_select 'h2.panel-title'
+        assert_select 'div.panel-body'
+  end
+  
+end
---
+++ test/integration/list_categories_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class CreateCategoriesTest < ActionDispatch::IntegrationTest
+ 
+  def setup # bevri kategoriistvis sachiroa setup (?)
+    @category = Category.create(name: "books")
+    @category2 = Category.create(name: "programming")  
+  end
+  
+
+  
+  
+  
+  test "should show categories listing" do
+    get categories_path
+    assert_template 'categories/index'
+    assert_select "a[href=?]", category_path(@category), text: @category.name   # vamowmebt link da misi texti sworia tu ara
+    assert_select "a[href=?]", category_path(@category2), text: @category2.name
+  end
+  
+  
+end
